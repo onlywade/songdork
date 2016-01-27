426e65311eaf52a9a07d32ae176124dab0c68e53
Wade Catron <wcatron@1056-wcatron.twilio.internal>
2016-01-27 09:52:11 -0800

    Improved metho naming and organization, add comments.

--- twongs.py
+++ twongs.py
@@ -18,64 +18,66 @@ T_SID = os.getenv('T_SID')
 T_AUTH_TOKEN = os.getenv('T_AUTH_TOKEN')
 T_FROM_NO = os.getenv('T_FROM_NO')
 
-app.logger.info('Environment loaded.')
+SONG_UNKNOWN_MESSAGE = 'Hmm, that doesn\'t sound at all familiar. Try singing it louder next time.'
 
 
-@app.route('/find_song', methods=['GET'])
-def find_song():
-    from_ = request.args.get('From')
-    lyrics = request.args.get('Body')
-    if lyrics and len(lyrics) > 5:
-        response_message = do_search_request(lyrics)
-    else:
-        response_message = 'that could be anything man'
+@app.route('/twilio_callback', methods=['GET'])
+def twilio_callback():
+    """
+    Main entrypoint. (Callback from Twilio upon receipt of SMS message.)
+    """
+    origin_number = request.args.get('From')    # number the SMS originated from
+    partial_lyrics = request.args.get('Body')   # body of SMS, presumed to be song lyrics
 
-    app.logger.info('Sending SMS response: {}'.format(response_message))
-    send_sms_response(from_, response_message)
+    try:
+        artist_and_song = find_artist_and_song(partial_lyrics)
+        response_message = message_for_artist_and_song(artist_and_song)
+    except Exception as e:
+        response_message = 'Sorry I am broken. {}'.format(e)
 
-    return response_message
+    send_sms(origin_number, response_message)
+
+    return response_message # it doesn't really matter how we respond to the Twilio callback
 
 
-def do_search_request(lyrics):
+def find_artist_and_song(lyrics):
+    """
+    Given a snippet of lyrics, searches lyricsfreak for a suitable match.
+    Returns an (artist, song) tuple, or None if no results found.
+    """
+
+    # google custom search engine API params
     params = {'key': G_API_KEY,
               'cx': G_CX_ID,
               'q': lyrics}
 
-    try:
-        response = requests.get(G_SEARCH_URL, params=params)
-    except Exception as e:
-        app.logger.error('Exception during search request: {}'.format(e))
-        return 'Sorry I am broken. {}'.format(e)
-
-    return parse_search_response(response)
-
-
-def parse_search_response(response):
+    response = requests.get(G_SEARCH_URL, params=params)
     results = json.loads(response.content.decode('utf-8'))
+
     items = results.get('items')
     if items:
         first_hit = items[0]['title']
-        message = respond_with_hit(first_hit)
+        return parse_result(first_hit)
     else:
-        message = 'Hmm, that doesn\'t sound like anything I know..'
-
-    app.logger.info('Search results resolved to: {}'.format(message))
-    return message
+        return None
 
 
-def respond_with_hit(result_string):
-    match = re.match('(.*)\ Lyrics\ -\ (.*)', result_string)
-    if len(match.groups()) == 2:
-        song = match.group(1)
-        artist = match.group(2)
-        message = get_random_template().format(song=song, artist=artist)
+def message_for_artist_and_song(artist_and_song):
+    """
+    Given an (artist, song) tuple, returns a nice message about it.
+    """
+    if artist_and_song:
+        artist = artist_and_song[0]
+        song = artist_and_song[1]
+        return get_random_message_template().format(artist=artist, song=song)
     else:
-        message = 'Hmm, that doesn\'t sound like anything I know..'
-
-    return message
+        return SONG_UNKNOWN_MESSAGE
 
 
-def get_random_template():
+def get_random_message_template():
+    """
+    Returns a random result message for interpolating artist/song into.
+    """
 
     templates = [
         'whoah dude i LOOOOVE {artist}! not that song though.',
@@ -89,7 +91,24 @@ def get_random_template():
     return random.choice(templates)
 
 
-def send_sms_response(to, message):
+def parse_result(result_string):
+    """
+    Given a result entry from the Google search API, returns an (artist, song) tuple or None
+    """
+
+    match = re.match('(.*)\ Lyrics\ -\ (.*)', result_string)
+    if len(match.groups()) == 2:
+        song = match.group(1)
+        artist = match.group(2)
+        return (artist, song)
+    else:
+        return None
+
+
+def send_sms(to, message):
+    """
+    Just sends a text.
+    """
     client = TwilioRestClient(T_SID, T_AUTH_TOKEN)
     client.messages.create(to=to, from_=T_FROM_NO, body=message)
 
